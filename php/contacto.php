cf480b3b2af41544236b6bbafae4951b0eeb225e
Schelotto, Jorge Alberto <jasfotografo@hotmail.com>
2020-02-22 20:43:19 -0300

    jasfotografo@hotmail.com

--- php/contacto.php
+++ php/contacto.php
@@ -5,11 +5,16 @@ if(isset( $_POST['email']))
 $email = $_POST['email'];
 if(isset( $_POST['subject']))
 $subject = 'Casamiento Andre y Leo';
-$message = ''
+$message = $_POST['message'];
 
 $content="From: $name \n Email: $email \n Message: $message";
 $recipient = "azcine@gmail.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
-header ("Location: Casamiento Andre y Leo");
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+mail($recipient, $subject, $content, $header, utf8_decode($message)) or die("Error!");
+header ("Location: index.html");
 ?>
